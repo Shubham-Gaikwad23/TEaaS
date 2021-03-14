1da7192db8e4499f25995699ba475ea176bb2a9a
Shubham_Gaikwad1 <shubham.gaikwad23@gmail.com>
2021-03-14 19:27:07 +0530

    publish test results

--- jobs/job1.groovy
+++ jobs/job1.groovy
@@ -14,7 +14,14 @@ node {
 	}
 	stage('Publish test results') {
 		echo 'Publishing test results'
-		httpRequest consoleLogResponseBody: true, ignoreSslErrors: true, outputFile: 'test_results.xml', responseHandle: 'NONE', url: 'https://gist.github.com/n1k0/4332371', wrapAsMultipart: false
+		//httpRequest consoleLogResponseBody: true, ignoreSslErrors: true, outputFile: 'test_results.xml', responseHandle: 'NONE', url: 'https://gist.github.com/n1k0/4332371', wrapAsMultipart: false
+		writeFile file: 'test_results.xml', text: '''<testsuite tests="3">
+    <testcase classname="foo1" name="ASuccessfulTest"/>
+    <testcase classname="foo2" name="AnotherSuccessfulTest"/>
+    <testcase classname="foo3" name="AFailingTest">
+        <failure type="NotEnoughFoo"> details about failure </failure>
+    </testcase>
+</testsuite>'''
 		junit '**test_results.xml'
 	}
 	stage('Clean up') {
