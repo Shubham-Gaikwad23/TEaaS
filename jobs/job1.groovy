1e58ff0c7baadff64697a519568a08d7507fa5b2
Shubham_Gaikwad1 <shubham.gaikwad23@gmail.com>
2021-03-14 19:18:09 +0530

    publish test results

--- jobs/job1.groovy
+++ jobs/job1.groovy
@@ -1,8 +1,6 @@
 @Library('TEaaS')
 import org.coep.*
 
-def stages = new Stages()
-
 node {
 	stage('Checkout SCM') {
 		echo 'Checking out SCM'
@@ -16,7 +14,8 @@ node {
 	}
 	stage('Publish test results') {
 		echo 'Publishing test results'
-		stages.publishTestResults()
+		httpRequest consoleLogResponseBody: true, ignoreSslErrors: true, outputFile: 'test_results.xml', responseHandle: 'NONE', url: 'https://gist.github.com/n1k0/4332371', wrapAsMultipart: false
+		junit '**test_results.xml'
 	}
 	stage('Clean up') {
 		echo 'Clearing workspace'
--- vars/stages.groovy
+++ vars/stages.groovy
@@ -3,12 +3,25 @@ import org.coep.*
 
 class Stages implements Serializable {
     Jenkins jenkins
+    def env
+    def steps
+    def buildParams
+    def currentBuild
 
-    Stages() {
+
+    Stages(env, steps, buildParams, currentBuild) {
+        this.env = env
+        this.steps = steps
+        this.buildParams = buildParams
+        this.currentBuild = currentBuild
         jenkins = new Jenkins()
     }
 
     void publishTestResults() {
         jenkins.publishTestResult()
+    }
+
+    Stages createInstance() {
+        return new Stages(env, steps, params, currentBuild)
     }
 }
