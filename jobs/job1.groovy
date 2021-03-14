463526e982fca66d2b03273ff59e03aa7739da94
Shubham_Gaikwad1 <shubham.gaikwad23@gmail.com>
2021-03-14 18:34:20 +0530

    wrap stages in stages{}

--- jobs/job1.groovy
+++ jobs/job1.groovy
@@ -2,23 +2,25 @@
 import org.coep.*
 
 node {
-	stage 'Checkout SCM' {
-		echo 'Checking out SCM'
-	}
-	stage 'Build' {
-		echo 'Compiling'
-		echo 'Building'
-	}
-	stage 'Run regression test' {
-		echo 'Running all tests'
-	}
-	stage 'Publish test results' {
-		echo 'Publishing test results'
-	}
-	stage 'Clean up' {
-		echo 'Clearing workspace'
-	}
-	stage 'Send Email' {
-		echo 'Sending test report via email'
+	stages {
+		stage 'Checkout SCM' {
+			echo 'Checking out SCM'
+		}
+		stage 'Build' {
+			echo 'Compiling'
+			echo 'Building'
+		}
+		stage 'Run regression test' {
+			echo 'Running all tests'
+		}
+		stage 'Publish test results' {
+			echo 'Publishing test results'
+		}
+		stage 'Clean up' {
+			echo 'Clearing workspace'
+		}
+		stage 'Send Email' {
+			echo 'Sending test report via email'
+		}
 	}
 }
