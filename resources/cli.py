76d9633596ff78ff5de3083c95b2e3a35880d886
Shubham_Gaikwad1 <shubham.gaikwad23@gmail.com>
2021-03-14 20:26:33 +0530

    cli

--- resources/cli.py
+++ resources/cli.py
@@ -1,6 +1,6 @@
 import jenkins
 
-server = jenkins.Jenkins('http://localhost:8080', username='jenkins', password='jenkins')
+server = jenkins.Jenkins('http://desktop-l5t987d.local:8080', username='jenkins', password='jenkins')
 user = server.get_whoami()
 version = server.get_version()
 print('Hello %s from Jenkins %s' % (user['fullName'], version))
